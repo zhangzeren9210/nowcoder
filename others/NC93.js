41f9ab73484f9bdfabd234a2930963b198b3dfd8
zhangzeren02 <>
2021-08-31 14:41:37 +0800

    93

---
+++ others/NC93.js
@@ -0,0 +1,90 @@
+/**
+ * lru design
+ * @param operators int整型二维数组 the ops
+ * @param k int整型 the k
+ * @return int整型一维数组
+ */
+function ListNode(key, val) {
+  this.val = val;
+  this.key = key;
+  this.prev = null;
+  this.next = null;
+}
+function LRU(operators, k) {
+  let size = 0;
+  let head = new ListNode(-1, -1);
+  let tail = new ListNode(-1, -1);
+  head.next = tail;
+  tail.prev = head;
+  let map = {};
+  let result = [];
+  for (let i = 0; i < operators.length; i++) {
+    const [operator, key, value] = operators[i];
+    if (operator === 1) {
+      set(key, value);
+    }
+    if (operator === 2) {
+      get(key);
+    }
+  }
+  function set(key, value) {
+    let node = map[key];
+    if (!node) {
+      let newnode = new ListNode(key, value);
+      map[key] = newnode;
+      addToHead(newnode);
+      size++;
+      if (size > k) {
+        let node = tail.prev;
+        removeNode(node);
+        delete map[node.key];
+        size--;
+      }
+    } else {
+      node.val = value;
+      addToHead(node);
+    }
+  }
+  function get(key) {
+    let node = map[key];
+    let tmp;
+    if (!node) {
+      tmp = -1;
+    } else {
+      moveToHead(node);
+      tmp = node.val;
+    }
+    result.push(tmp);
+    return tmp;
+  }
+  function moveToHead(node) {
+    removeNode(node);
+    addToHead(node);
+  }
+  function removeNode(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+  }
+  function addToHead(node) {
+    node.prev = head;
+    node.next = head.next;
+    head.next.prev = node;
+    head.next = node;
+  }
+  return result;
+}
+module.exports = {
+  LRU: LRU,
+};
+
+function ReverseList(pHead) {
+  let prev = null,
+    curr = pHead;
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
